9db3dd5d1d775db17f09007508b96e1fcc70e23c
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-01-02 13:01:42 +0100

    Fixet Burger menu

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const bubbles = document.querySelector(".bubble");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    bar1.classList.toggle("active");
+    bar2.classList.toggle("active");
+    bar3.classList.toggle("active");
+  });
+  
+  document.querySelectorAll(".nav-link").forEach((n) =>
+    n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+  
+
 window.onscroll = function () {
     scrollRotate();
 };
